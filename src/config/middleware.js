a35111e8d0f5240e438cace52f86fd43e07644d5
Diego Lorenesi <drlorenesi@gmail.com>
2022-12-01 17:07:24 -0600

    Correct test URL.

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     origin,
     'http://localhost:3000',
-    'https://deltacold.com/',
+    'https://deltacold.com',
     'https://granada-frontend.pages.dev',
   ],
   credentials: true,
