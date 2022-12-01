8d8c2c4da1ca34599df75adc485e46fe0ab4558b
Diego Lorenesi <drlorenesi@gmail.com>
2022-12-01 09:53:35 -0600

    Corregir CORS de Beta.

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     origin,
     'http://localhost:3000',
-    'https://granada-frontend.pages.dev/',
+    'https://granada-frontend.pages.dev',
   ],
   credentials: true,
 };
