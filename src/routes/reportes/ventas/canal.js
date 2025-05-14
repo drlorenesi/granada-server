d3f3182db393b4faebfeabee543e064118fcd003
Diego Lorenesi <drlorenesi@gmail.com>
2025-05-14 15:47:53 -0600

    Refactor SQL query in canal.js for improved readability and performance

--- src/routes/reportes/ventas/canal.js
+++ src/routes/reportes/ventas/canal.js
@@ -23,78 +23,64 @@ router.get(
   async (req, res) => {
     const { fechaIni, fechaFin } = req.query;
     const { duration, rows } = await query(`
-    DECLARE
-        @FechaIni DATE = '${fechaIni}',
-        @FechaFin DATE = '${fechaFin}'
-        SELECT
-            T1.[Descripcion General] AS 'canal',
-            ISNULL(T2.[Total Ventas sIVA], 0) AS 'total_ventas_siva',
-            ISNULL(T4.[Total NC Valor sIVA], 0) AS 'total_nc_valor_siva',
-            ISNULL(T2.[Total Ventas sIVA], 0) - ISNULL(T4.[Total NC Valor sIVA], 0) AS 'total' -- DIVISION CLIENTE
-            FROM
-            (
-                SELECT
-                DV.[Descripcion General],
-                DV.Orden
-                FROM
-                [DIVISION CLIENTE] AS DV
-            ) AS T1 -- FACTURAS GRABADAS sIVA
-            LEFT JOIN (
-                SELECT
-                DV.[Descripcion General],
-                ISNULL(
-                    ROUND(
-                    SUM(
-                        CASE
-                        WHEN FM.Moneda = 1 THEN FM.Total / 1.12
-                        WHEN FM.Moneda != 1 THEN FM.Total * FM.[Tipo Cambio]
-                        END
-                    ),
-                    2
-                    ),
-                    0
-                ) AS 'Total Ventas sIVA'
-                FROM
-                [FACTURA MAESTRO] AS FM
-                LEFT JOIN [DIVISION CLIENTE] AS DV ON FM.[Division Cliente] = DV.Codigo
-                WHERE
-                FM.Empresa = 1
-                AND CAST(FM.Fecha AS DATE) BETWEEN @FechaIni
-                AND @FechaFin
-                AND FM.Estatus = 'G'
-                GROUP BY
-                DV.[Descripcion General]
-            ) AS T2 ON T1.[Descripcion General] = T2.[Descripcion General] -- NOTAS DE CRÉDITO DEVOLUCION sIVA
-            -- NOTAS DE CRÉDITO VALOR sIVA
-            LEFT JOIN (
-                SELECT
-                DC.[Descripcion General],
+    DECLARE @FechaIni DATE = '${fechaIni}',
+            @FechaFin DATE = '${fechaFin}'
+      SELECT
+        DV.[Descripcion General] AS 'canal',
+        ISNULL(
+          (
+            SELECT
+              SUM(
                 ROUND(
-                    SUM(
-                    CASE
-                        WHEN CXC.Moneda = 1 THEN CXC.Total / 1.12
-                        WHEN CXC.Moneda != 1 THEN CXC.Total * CXC.[Tipo Cambio]
-                    END
-                    ),
-                    2
-                ) AS 'Total NC Valor sIVA'
-                FROM
-                [CXC MAESTRO] AS CXC
-                LEFT JOIN CLIENTE AS C ON CXC.Cliente = C.Codigo
-                AND CXC.Empresa = C.Empresa
-                LEFT JOIN [DIVISION CLIENTE] AS DC ON C.Division = DC.Codigo
-                WHERE
-                CXC.Empresa = 1
-                AND CXC.Tipo = 2
-                AND CAST(CXC.Fecha AS DATE) BETWEEN @FechaIni
-                AND @FechaFin
-                AND CXC.Estatus = 'G'
-                GROUP BY
-                DC.[Descripcion General]
-            ) AS T4 ON T1.[Descripcion General] = T4.[Descripcion General]
-            ORDER BY
-            T1.Orden
-    `);
+                  CASE
+                    WHEN FM.Moneda = 1 THEN FM.Total / 1.12
+                    WHEN FM.Moneda != 1 THEN FM.Total * FM.[Tipo Cambio]
+                  END,
+                  2
+                )
+              )
+            FROM
+              [FACTURA MAESTRO] AS FM
+              INNER JOIN CLIENTE AS C ON C.Empresa = FM.Empresa
+              AND FM.Cliente = C.Codigo
+            WHERE
+              FM.Empresa = 1
+              AND CONVERT(DATE, FM.Fecha) BETWEEN @FechaIni AND @FechaFin
+              AND FM.Estatus = 'G'
+              AND C.Division = DV.Codigo
+          ),
+          0
+        ) AS 'Total Ventas sIVA',
+        ISNULL(
+          (
+            SELECT
+              SUM(
+                ROUND(
+                  CASE
+                    WHEN CXC.Moneda = 1 THEN CXC.Total / 1.12
+                    WHEN CXC.Moneda != 1 THEN CXC.Total * CXC.[Tipo Cambio]
+                  END,
+                  2
+                )
+              )
+            FROM
+              [CXC MAESTRO] AS CXC
+              LEFT JOIN CLIENTE AS C ON CXC.Cliente = C.Codigo
+              AND CXC.Empresa = C.Empresa
+            WHERE
+              CXC.Empresa = 1
+              AND CXC.Tipo = 2
+              AND CONVERT(DATE, CXC.Fecha) BETWEEN @FechaIni AND @FechaFin
+              AND CXC.Estatus = 'G'
+              AND C.Division = DV.Codigo
+          ),
+          0
+        ) 'Total NC Valor sIVA'
+      FROM
+        [DIVISION CLIENTE] AS DV
+      ORDER BY
+        DV.Orden
+  `);
     res.send({ duration, query: req.query, rows });
   }
 );
